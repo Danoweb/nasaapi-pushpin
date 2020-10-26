323d3b37be836b1e9a1aa867b57851749f7e13fd
Daniel Reed <daniel.reed@danowebstudios.com>
2020-10-25 23:14:58 -0400

    Feat(nasaapi): updated to the nasa API for the api server data, updated Docker and python requirements

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# trackergg-pushpin
+# nasa-pushpin
 A TrackerGG api example using Push-pin for streaming Realtime updates from the TrackerGG API.
 
 
-## You will need a Tracker.GG Api Account
-* Register Here: https://thetrackernetwork.com/auth/register?domain=account.tracker.gg
-* Create an App and get an API KEY here: https://tracker.gg/developers/apps/create
+## You will need a NASA Api Account
+* Register Here: https://api.nasa.gov/
 
-## TrackerGG Client Container
-A simple python app which makes requests to the Tracker GG API.
+
+## Nasa Client Container
+A simple python app which makes requests to the NASA API. https://api.nasa.gov/
+
+* Add your API_KEY to the `creds.env` file
+
+* Serves port `5000`
+
+### Endpoints
+
+* `/` - health check /heartbeat, responds if container is online and reachable.
+* `/stats` - gives the last 5 days of Near Earth Orbit Object data from NASA.
 
 ## Pushpin Container
 A pushpin implementation which streams results of the trackerGG api to any streams connected.
---
+++ nasaapi_client/index.py
@@ -0,0 +1,39 @@
+from flask import Flask, Response, request
+from datetime import datetime, timedelta
+import requests, os, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    resp = Response('{"message": "Client Online"}')
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+
+@app.route('/stats')
+def player_stats():
+    data = {}
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    start_date = datetime.now() - timedelta(days=-7)
+    start_date = start_date.strftime('%Y-%m-%d')
+    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={os.environ.get('API_KEY')}"
+
+    payload = {}
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    api_values = json.loads(response.text)
+
+    data = {
+        "details": api_values
+    }
+
+    resp = Response(json.dumps(data))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
